d7653ec280e3448f28bfc70e9e948749d9a3d2ca
David Ding <ding@dingstudio.cn>
2017-08-29 10:25:48 +0800

    update index.php by default page

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php
-header('Content-Type: text/plain; charset=UTF-8');
-$response = array(
-	'code'	=>	503,
-	'message'	=>	'站点建设中，暂不开放。',
-	'requestId'	=>	date('YmdHis', time())
-);
-echo json_encode($response,JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
+header('Content-Type: text/html; charset=UTF-8');
+require_once(dirname(__FILE__).'/index.html');
